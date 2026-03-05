968e72c677d5611d7dbf5434fac391b92201d025
Javier <javier.castillo239873@potros.itson.edu.mx>
2026-03-04 19:29:28 -0700

    Actividad6

--- app/src/main/java/mx/itson/edu/practica5/SeatSelection.kt
+++ app/src/main/java/mx/itson/edu/practica5/SeatSelection.kt
@@ -2,67 +2,86 @@ package mx.itson.edu.practica5
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.inappmessaging.model.Text
 
 class SeatSelection : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_selection)
 
-        val title: TextView = findViewById<TextView>(R.id.titleSeats)
-        var posMovie=-1
+        val title: TextView = findViewById(R.id.titleSeats)
+        val confirm: Button = findViewById(R.id.confirm)
 
-        val bundle= intent.extras
-        if(bundle!=null){
-            title.setText(bundle.getString("name"))
-            posMovie=bundle.getInt("id")
+        var posMovie = -1
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            title.text = bundle.getString("name")
+            posMovie = bundle.getInt("id")
         }
 
-        val confirm: Button = findViewById<Button>(R.id.confirm)
+        val row1: RadioGroup = findViewById(R.id.row1)
+        val row2: RadioGroup = findViewById(R.id.row2)
+        val row3: RadioGroup = findViewById(R.id.row3)
+        val row4: RadioGroup = findViewById(R.id.row4)
+
+        val rows = listOf(row1, row2, row3, row4)
+
+        row1.setOnCheckedChangeListener { _, checkedId -> if (checkedId > -1) {
+            row2.clearCheck()
+            row3.clearCheck()
+            row4.clearCheck()
+        }
+        }
+
+        row2.setOnCheckedChangeListener { _, checkedId -> if (checkedId > -1) {
+            row1.clearCheck()
+            row3.clearCheck()
+            row4.clearCheck()
+        }
+        }
+
+        row3.setOnCheckedChangeListener { _, checkedId -> if (checkedId > -1) {
+            row1.clearCheck()
+            row2.clearCheck()
+            row4.clearCheck()
+        }
+        }
+
+        row4.setOnCheckedChangeListener { _, checkedId -> if (checkedId > -1) {
+            row1.clearCheck()
+            row2.clearCheck()
+            row3.clearCheck()
+        }
+        }
+
         confirm.setOnClickListener {
-            Toast.makeText(this,"Enjoy the movie! :D",Toast.LENGTH_LONG).show()
-        }
 
-        val row1: RadioGroup = findViewById<RadioGroup>(R.id.row1)
-        val row2: RadioGroup = findViewById<RadioGroup>(R.id.row2)
-        val row3: RadioGroup = findViewById<RadioGroup>(R.id.row3)
-        val row4: RadioGroup = findViewById<RadioGroup>(R.id.row4)
+            var selectedRadio: RadioButton? = null
 
-        row1.setOnCheckedChangeListener { group, checkedId ->
-            if(checkedId>-1){
-                row2.clearCheck()
-                row3.clearCheck()
-                row4.clearCheck()
+            for (row in rows) {
+                if (row.checkedRadioButtonId != -1) {
+                    selectedRadio = findViewById(row.checkedRadioButtonId)
+                    break
+                }
             }
-        }
-        row2.setOnCheckedChangeListener { group, checkedId ->
-            if(checkedId>-1){
-                row1.clearCheck()
-                row3.clearCheck()
-                row4.clearCheck()
 
-                row2.check(checkedId)
-            }
-        }
-        row3.setOnCheckedChangeListener { group, checkedId ->
-            if(checkedId>-1){
-                row2.clearCheck()
-                row1.clearCheck()
-                row4.clearCheck()
+            if (selectedRadio != null) {
 
-                row3.check(checkedId)
-            }
-        }
-        row4.setOnCheckedChangeListener { group, checkedId ->
-            if(checkedId>-1){
-                row2.clearCheck()
-                row3.clearCheck()
-                row1.clearCheck()
+                selectedRadio.isEnabled = false
+                selectedRadio.setBackgroundResource(R.drawable.radio_disabled)
 
-                row4.check(checkedId)
+                Toast.makeText(this, "Enjoy the movie! :D", Toast.LENGTH_LONG).show()
+
+
+            } else {
+                Toast.makeText(this, "Select a seat first", Toast.LENGTH_SHORT).show()
             }
         }
     }
