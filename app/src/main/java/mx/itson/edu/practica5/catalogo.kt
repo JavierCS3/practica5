ad6a94e43a4ed402105ab2866c442bad46c4640e
Javier <javier.castillo239873@potros.itson.edu.mx>
2026-03-04 17:54:25 -0700

    Actividad6

--- app/src/main/java/mx/itson/edu/practica5/catalogo.kt
+++ app/src/main/java/mx/itson/edu/practica5/catalogo.kt
@@ -39,21 +39,24 @@ class catalogo : AppCompatActivity()    {
     }
 
     fun cargarPeliculas() {
-        peliculas.add(Pelicula("Demon Slayer: Kimetsu no Yaiba -To the Hashira Training", R.drawable.demon, R.drawable.demo,  "Kimetsu no Yaiba -To the Hashira Training- proyectará por primera vez en cines el episodio 11 del Arco de la Aldea de los Herreros, mostrando así la conclusión de la feroz batalla entre Tanjiro y la Cuarta Luna Creciente, Hatengu, además de cómo Nezuko logra caminar bajo el sol. Le seguirá el episodio 1 del Arco del Entrenamiento de los Pilares, donde veremos el inicio del entrenamiento de los Pilares para prepararse de cara a la próxima batalla contra Muzan Kibutsuji, que se podrá ver por primera vez."))
-        peliculas.add(Pelicula("Dune", R.drawable.dune, R.drawable.dune2,"Duna: Parte Dos\" explorará el viaje mítico de Paul Atreides mientras se une con Chani y los Fremen mientras está en un camino de venganza contra los conspiradores que destruyeron a su familia. Enfrentando una elección entre el amor de su vida y el destino del universo conocido, se esfuerza por evitar un futuro terrible que solo él pueda prever."))
-        peliculas.add(Pelicula("Ghostbusters apocalipsis", R.drawable.ghostbusters, R.drawable.ghostbusters, "Duna: Parte Dos explorará el viaje mítico de Paul Atreides mientras se une con Chani y los Fremen mientras está en un camino de venganza contra los conspiradores que destruyeron a su familia. Enfrentando una elección entre el amor de su vida y el destino del universo conocido, se esfuerza por evitar un futuro terrible que solo él pueda prever."))
-        peliculas.add(Pelicula("Héroe Por Encargo", R.drawable.heroexencargo, R.drawable.heroeencargo, "Un ex agente de las fuerzas especiales acepta un trabajo para brindar seguridad a una periodista mientras entrevista a un dictador, pero estalla un golpe militar en medio de la entrevista y se ven obligados a escapar a la jungla donde deben sobrevivir."))
-        peliculas.add(Pelicula("Madame Web", R.drawable.madameweb, R.drawable.madame,"“Mientras tanto, en otro universo…” En un cambio del típico género, Madame Web cuenta la historia independiente del origen de una de las heroínas más enigmáticas de la editorial Marvel. El thriller de suspenso protagonizado por Dakota Johnson como Cassandra Webb, una paramédico de Manhattan que puede tener habilidades clarividentes. Forzada a enfrentarse a revelaciones de su pasado, ella forja una relación con tres mujeres jóvenes destinadas a tener poderosos futuros… si pueden sobrevivir a un presente mortal.\n"))
-        peliculas.add(Pelicula("Vidas Pasadas", R.drawable.vidaspasadas, R.drawable.vidaspasada, "Nora y Hae Sung, dos amigos de la infancia profundamente unidos, son separados después de que la familia de Nora emigrara de Corea del Sur. Veinte años después, se reúnen durante una fatídica semana para enfrentarse al amor y al destino."))
+
+
+
+        peliculas.add(Pelicula("Demon Slayer: Kimetsu no Yaiba -To the Hashira Training", R.drawable.demon, R.drawable.demo,  "Kimetsu no Yaiba -To the Hashira Training- proyectará por primera vez en cines el episodio 11 del Arco de la Aldea de los Herreros, mostrando así la conclusión de la feroz batalla entre Tanjiro y la Cuarta Luna Creciente, Hatengu, además de cómo Nezuko logra caminar bajo el sol. Le seguirá el episodio 1 del Arco del Entrenamiento de los Pilares, donde veremos el inicio del entrenamiento de los Pilares para prepararse de cara a la próxima batalla contra Muzan Kibutsuji, que se podrá ver por primera vez.",arrayListOf<Cliente>()))
+        peliculas.add(Pelicula("Dune", R.drawable.dune, R.drawable.dune2,"Duna: Parte Dos\" explorará el viaje mítico de Paul Atreides mientras se une con Chani y los Fremen mientras está en un camino de venganza contra los conspiradores que destruyeron a su familia. Enfrentando una elección entre el amor de su vida y el destino del universo conocido, se esfuerza por evitar un futuro terrible que solo él pueda prever.",arrayListOf<Cliente>()))
+        peliculas.add(Pelicula("Ghostbusters apocalipsis", R.drawable.ghostbusters, R.drawable.ghostbusters, "Duna: Parte Dos explorará el viaje mítico de Paul Atreides mientras se une con Chani y los Fremen mientras está en un camino de venganza contra los conspiradores que destruyeron a su familia. Enfrentando una elección entre el amor de su vida y el destino del universo conocido, se esfuerza por evitar un futuro terrible que solo él pueda prever.",arrayListOf<Cliente>()))
+        peliculas.add(Pelicula("Héroe Por Encargo", R.drawable.heroexencargo, R.drawable.heroeencargo, "Un ex agente de las fuerzas especiales acepta un trabajo para brindar seguridad a una periodista mientras entrevista a un dictador, pero estalla un golpe militar en medio de la entrevista y se ven obligados a escapar a la jungla donde deben sobrevivir.",arrayListOf<Cliente>()))
+        peliculas.add(Pelicula("Madame Web", R.drawable.madameweb, R.drawable.madame,"“Mientras tanto, en otro universo…” En un cambio del típico género, Madame Web cuenta la historia independiente del origen de una de las heroínas más enigmáticas de la editorial Marvel. El thriller de suspenso protagonizado por Dakota Johnson como Cassandra Webb, una paramédico de Manhattan que puede tener habilidades clarividentes. Forzada a enfrentarse a revelaciones de su pasado, ella forja una relación con tres mujeres jóvenes destinadas a tener poderosos futuros… si pueden sobrevivir a un presente mortal.\n",arrayListOf<Cliente>()))
+        peliculas.add(Pelicula("Vidas Pasadas", R.drawable.vidaspasadas, R.drawable.vidaspasada, "Nora y Hae Sung, dos amigos de la infancia profundamente unidos, son separados después de que la familia de Nora emigrara de Corea del Sur. Veinte años después, se reúnen durante una fatídica semana para enfrentarse al amor y al destino.",arrayListOf<Cliente>()))
     }
 
     fun cargarSeries() {
-        series.add(Pelicula("Avatar: La leyenda de Aang", R.drawable.ant, R.drawable.ant,"La leyenda de Aang sigue al último sobreviviente de los Nómadas del Aire, quien deberá restaurar el equilibrio en el mundo entre las tres naciones restantes: la Tribu del Agua, el Reino Tierra y la Nación del Fuego.\n"))
-        series.add(Pelicula("Halo", R.drawable.halo, R.drawable.halos, "Una evacuación mortal cambia la guerra del Jefe Maestro con el Covenant. En Reach, un nuevo régimen toma el mando en FLEETCOM. John siente una amenaza inminente cerca. Estreno de temporada.\n"))
-        series.add(Pelicula("Leveling", R.drawable.sololeveling, R.drawable.sololevelings, "En un mundo en el que ciertos humanos llamados “cazadores” poseen habilidades mágicas, estos deben luchar contra monstruos para proteger a la raza humana de una aniquilación segura. Un cazador muy débil llamado Sung Jinwoo se encuentra en una lucha en la que solo puede tratar de sobrevivir.\n"))
-        series.add(Pelicula("Mi adorable demonio", R.drawable.adorabledemonio, R.drawable.adorabledemonios,"Se centra en la vida de Jung Koo Won, un temerario demonio que pierde sus poderes tras conocer a Do Do-Hee, una exitosa empresaria con la que deberá de colaborar para recuperarlos.\n"))
-        series.add(Pelicula("El Monstruo de la Vieja Seúl", R.drawable.el_monstruo, R.drawable.elmonstruovieja, "Gyeongseong, 1945. En la oscura era colonial de Seúl, un empresario y una investigadora luchan por sobrevivir y se enfrentan a un monstruo nacido de la avaricia humana."))
-        series.add(Pelicula("Witcher", R.drawable.thewitcher, R.drawable.thewitchers,  "Geralt de Rivia, un cazador de monstruos mutante, viaja en pos de su destino por un mundo turbulento en el que, a menudo, los humanos son peores que las bestias."))
+        series.add(Pelicula("Avatar: La leyenda de Aang", R.drawable.ant, R.drawable.ant,"La leyenda de Aang sigue al último sobreviviente de los Nómadas del Aire, quien deberá restaurar el equilibrio en el mundo entre las tres naciones restantes: la Tribu del Agua, el Reino Tierra y la Nación del Fuego.\n",arrayListOf<Cliente>()))
+        series.add(Pelicula("Halo", R.drawable.halo, R.drawable.halos, "Una evacuación mortal cambia la guerra del Jefe Maestro con el Covenant. En Reach, un nuevo régimen toma el mando en FLEETCOM. John siente una amenaza inminente cerca. Estreno de temporada.\n",arrayListOf<Cliente>()))
+        series.add(Pelicula("Leveling", R.drawable.sololeveling, R.drawable.sololevelings, "En un mundo en el que ciertos humanos llamados “cazadores” poseen habilidades mágicas, estos deben luchar contra monstruos para proteger a la raza humana de una aniquilación segura. Un cazador muy débil llamado Sung Jinwoo se encuentra en una lucha en la que solo puede tratar de sobrevivir.\n",arrayListOf<Cliente>()))
+        series.add(Pelicula("Mi adorable demonio", R.drawable.adorabledemonio, R.drawable.adorabledemonios,"Se centra en la vida de Jung Koo Won, un temerario demonio que pierde sus poderes tras conocer a Do Do-Hee, una exitosa empresaria con la que deberá de colaborar para recuperarlos.\n",arrayListOf<Cliente>()))
+        series.add(Pelicula("El Monstruo de la Vieja Seúl", R.drawable.el_monstruo, R.drawable.elmonstruovieja, "Gyeongseong, 1945. En la oscura era colonial de Seúl, un empresario y una investigadora luchan por sobrevivir y se enfrentan a un monstruo nacido de la avaricia humana.",arrayListOf<Cliente>()))
+        series.add(Pelicula("Witcher", R.drawable.thewitcher, R.drawable.thewitchers,  "Geralt de Rivia, un cazador de monstruos mutante, viaja en pos de su destino por un mundo turbulento en el que, a menudo, los humanos son peores que las bestias.",arrayListOf<Cliente>()))
     }
 
     class PeliculaAdapter : BaseAdapter{
@@ -78,25 +81,37 @@ class catalogo : AppCompatActivity()    {
             return p0.toLong()
         }
 
-        override fun getView(pe: Int, p1: View?, p2: ViewGroup?): View {
+        override fun getView(pe: Int, conterView: View?, p2: ViewGroup?): View {
 
             var pelicula = peliculas[pe]
-
             var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             var vista = inflator.inflate(R.layout.pelicula, null)
-
-            var image: ImageView = vista.findViewById(R.id.image_movie_cell)
-            var title: TextView = vista.findViewById(R.id.movie_title_cell)
+            var image: ImageView=vista.findViewById<ImageView>(R.id.image_movie_cell)
+            var title: TextView=vista.findViewById<TextView>(R.id.movie_title_cell)
 
             image.setImageResource(pelicula.image)
             title.setText(pelicula.titulo)
+//            vista.iv_peliculas.setImageResorce(pelicula.image)
+//            vista.tv_titulos.setText(pelicula.titulo)
 
-            image.setOnClickListener { v: View ->
+//            vista.iv_peliculas.setOnClickListener(){
+//                val intento = Intent(context, detalle_pelicula::class.java)
+//                intento.putExtra("titulo", pelicula.titulo)
+//                intento.putExtra("image", pelicula.image)
+//                intento.putExtra("header", pelicula.header)
+//                intento.putExtra("sinopsis", pelicula.sinopsis)
+//                intento.putExtra("numberSeats",(20-pelicula.seats.size))
+//
+//                context!!.startActivity(intento)
+//            }
+
+            image.setOnClickListener(){
                 val intento = Intent(context, detalle_pelicula::class.java)
                 intento.putExtra("titulo", pelicula.titulo)
                 intento.putExtra("image", pelicula.image)
                 intento.putExtra("header", pelicula.header)
                 intento.putExtra("sinopsis", pelicula.sinopsis)
+                intento.putExtra("seats",(20-pelicula.seats.size))
 
                 context!!.startActivity(intento)
             }
--- app/src/main/java/mx/itson/edu/practica5/detalle_pelicula.kt
+++ app/src/main/java/mx/itson/edu/practica5/detalle_pelicula.kt
@@ -1,28 +1,41 @@
 package mx.itson.edu.practica5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class detalle_pelicula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_detalle_pelicula)
 
         val iv_pelicula_image: ImageView = findViewById<ImageView>(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById<TextView>(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById<TextView>(R.id.tv_pelicula_desc)
+        val seatsLeft: TextView = findViewById<TextView>(R.id.seatLeft)
+        val buyTickets: Button = findViewById<Button>(R.id.buyTickets)
 
         val bundle=intent.extras
         if(bundle!=null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+
+            val seats = bundle.getInt("seats")
+            seatsLeft.text = "Seats available: $seats"
+
+            buyTickets.setOnClickListener {
+
+                val intent = Intent(this, SeatSelection::class.java)
+
+                intent.putExtra("titulo", bundle.getString("titulo"))
+                intent.putExtra("seats", seats)
+
+                startActivity(intent)
+            }
         }
     }
 }
